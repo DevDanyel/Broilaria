160c84ad010f75451fe8f9ac597a89522918c6d6
Jayla House <housejayla6115@gmail.com>
2023-11-09 14:37:22 -0600

    ProgressBar Is On Object Now
    -needs to not twirl
    -progress should not rotate with the onion
    -still want onion to rotate when not being held and not on chopping
    -progress bar is a bit iffy *I think its visual problem not coding*

--- Chad's CrabGame/Assets/scripts/IngrediantsObject.cs
+++ Chad's CrabGame/Assets/scripts/IngrediantsObject.cs
@@ -1,16 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Ingrediants")]
 public class IngrediantsObject : ScriptableObject
 {
     [SerializeField] public Mesh unchoppedMesh;
     [SerializeField] public Mesh choppedMesh;
-    
+    [SerializeField] public float totalChopTime;
+    [SerializeField] public float currentChopTimeStatus;
+    [SerializeField] public bool chopStatus;
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
 
-    private void Awake(){}
+    [SerializeField] public float currentSliderStatus;
+
+    private void Awake(){
+        currentChopTimeStatus = 0;
+        chopStatus=false;
+        currentSliderStatus = 0;
+    }
+
+    public void SetMaxSlider(float chop)
+	{
+		slider.maxValue = chop;
+		slider.value = chop;
+
+		fill.color = gradient.Evaluate(1f);
+	}
+
+    public void SetSlider(float chop)
+	{
+		slider.value = chop;
+
+		fill.color = gradient.Evaluate(slider.normalizedValue);
+	}
     private void OnEnable(){}
     private void OnDisable(){}
     private void OnDestroy(){}
--- Chad's CrabGame/Assets/scripts/onion.cs
+++ Chad's CrabGame/Assets/scripts/onion.cs
@@ -1,19 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class onion : MonoBehaviour
 {
     public IngrediantsObject IngrediantsObject;
     public MeshFilter onionMeshFilter;
 
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
     void Start(){
+        IngrediantsObject.chopStatus = false;
+        IngrediantsObject.slider = slider;
+        IngrediantsObject.gradient = gradient;
+        IngrediantsObject.fill = fill;
         onionMeshFilter = this.gameObject.GetComponent<MeshFilter>();
+        onionMeshFilter.mesh = IngrediantsObject.unchoppedMesh;
+        IngrediantsObject.totalChopTime = 120f;
+        IngrediantsObject.SetMaxSlider(IngrediantsObject.totalChopTime);
+        IngrediantsObject.currentChopTimeStatus = 0;
+        IngrediantsObject.currentSliderStatus = 0;
+        IngrediantsObject.SetSlider(0);
+    }
+
+    void Update(){
+
     }
 
     public void ChangeMeshToChopped(){
         Debug.Log("changing mesh");
         onionMeshFilter.mesh = IngrediantsObject.choppedMesh;
     }
+
+    public void IncreaseCutStatus(){
+        if(IngrediantsObject.totalChopTime >= IngrediantsObject.currentChopTimeStatus){
+            IngrediantsObject.currentChopTimeStatus +=10f;
+            IngrediantsObject.SetSlider(IngrediantsObject.currentChopTimeStatus);
+            StartCoroutine(ExampleCoroutine());
+        }else{
+            IngrediantsObject.chopStatus=true;
+            //ChangeMeshToChopped();
+
+        }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+       // Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+
 
 }
